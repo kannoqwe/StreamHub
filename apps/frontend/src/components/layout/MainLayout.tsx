15cc4235453c478e6df54af9b23c4e2c4216c05e
kannoqwe <pyntoxfejk@gmail.com>
2025-12-13 12:52:05 +0100

    feat(front): add Navbar layout

--- apps/frontend/src/components/layout/MainLayout.tsx
+++ apps/frontend/src/components/layout/MainLayout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '@components/layout/Navbar';
+import { useState } from 'react';
 
 export const MainLayout = () => {
+    const [sidebarOpen, setSidebarOpen] = useState(true);
+
     return (
         <div className="dark min-h-screen bg-zinc-950 text-zinc-100">
-            <Navbar toggleSidebar={() => {}} />
+            <Navbar
+                toggleSidebar={() => {
+                    setSidebarOpen(!sidebarOpen);
+                }}
+            />
 
             <main className="pt-14 min-h-screen">
                 <Outlet />
