fc6e31d0a7f131c0827e6d3d35e432d6a70d7be4
kannoqwe <pyntoxfejk@gmail.com>
2025-12-27 18:04:42 +0100

    feat(front): implement refresh token

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
 export const $api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: `${import.meta.env.VITE_API_URL}/auth/refresh`,
     headers: {
         'Content-Type': 'application/json',
     },
@@ -14,3 +14,32 @@ $api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     }
     return config;
 });
+
+$api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._isRetry) {
+            originalRequest._isRetry = true;
+            try {
+                const response = await axios.get(
+                    `${import.meta.env.VITE_API_URL}/auth/refresh`,
+                    {
+                        withCredentials: true,
+                    },
+                );
+
+                const newToken = response.data.accessToken;
+                localStorage.setItem('token', newToken);
+
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                return $api.request(originalRequest);
+            } catch (reAuthError) {
+                localStorage.removeItem('token');
+                return Promise.reject(reAuthError);
+            }
+        }
+        return Promise.reject(error);
+    },
+);
