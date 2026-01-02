8beb6369cf7bc1edf9fc87ebfc0e660060b97b43
kannoqwe <pyntoxfejk@gmail.com>
2026-01-02 18:48:48 +0100

    feat(shared): add category mdoel

--- packages/shared/api/models/index.ts
+++ packages/shared/api/models/index.ts
@@ -1,2 +1,3 @@
 export * from './user.model';
 export * from './stream.model';
+export * from './category.model';
--- packages/shared/api/models/stream.model.ts
+++ packages/shared/api/models/stream.model.ts
@@ -1,11 +1,12 @@
 import { UserProfile } from "./user.model";
+import { Category } from "./category.model";
 
 export interface Stream {
     id: number;
     title: string;
     thumbnail?: string | null;
     viewerCount: number;
-    category: number;
+    category: Category;
     tags?: string[];
     startedAt: number;
     streamer: UserProfile;
