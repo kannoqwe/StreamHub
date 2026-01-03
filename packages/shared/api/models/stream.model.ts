61ec32a63b43467b20f69d852c93493aa26ceeaa
kannoqwe <pyntoxfejk@gmail.com>
2026-01-03 16:44:56 +0100

    restore(shared): back to previous commit

--- packages/shared/api/models/stream.model.ts
+++ packages/shared/api/models/stream.model.ts
@@ -1,12 +1,11 @@
 import { UserProfile } from "./user.model";
-import { Category } from "./category.model";
 
 export interface Stream {
     id: number;
     title: string;
     thumbnail?: string | null;
     viewerCount: number;
-    category: Category;
+    category: number;
     tags?: string[];
     startedAt: number;
     streamer: UserProfile;
