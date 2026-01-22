cf8408df9daad0ac0997cf279585ffa3474f522f
kannoqwe <pyntoxfejk@gmail.com>
2026-01-22 16:21:13 +0100

    fix(back/user): fix service func

--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ChannelData } from '@streamhub/shared';
 import { UserService } from '@modules/user/user.service';
+import { ChannelService } from '@modules/user/channel.service';
 
 @Controller('User')
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private channelService: ChannelService,
+    ) {}
 
     @Get(':username')
     async getChannelData(
         @Param('username') username: string,
     ): Promise<ChannelData> {
-        return this.userService.findChannelData(username);
+        return this.channelService.findChannelData(username);
     }
 }
