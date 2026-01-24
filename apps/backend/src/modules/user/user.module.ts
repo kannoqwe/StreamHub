405721ef955b95630ba674f5064f1f176c184510
kannoqwe <pyntoxfejk@gmail.com>
2026-01-24 13:28:24 +0100

    fix(back): fix module import

--- apps/backend/src/modules/user/user.module.ts
+++ apps/backend/src/modules/user/user.module.ts
@@ -3,10 +3,9 @@ import { UserService } from './user.service';
 import { UserRepository } from '@modules/user/user.repository';
 import { RedisModule } from '@modules/redis/redis.module';
 import { UserController } from '@modules/user/user.controller';
-import { StreamModule } from '@modules/stream/stream.module';
 
 @Module({
-    imports: [RedisModule, StreamModule],
+    imports: [RedisModule],
     controllers: [UserController],
     providers: [UserService, UserRepository],
     exports: [UserService, UserRepository],
