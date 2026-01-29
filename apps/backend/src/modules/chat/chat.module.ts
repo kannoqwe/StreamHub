b86d8937fb5506ed03efc2a1fbe059b019561346
kannoqwe <pyntoxfejk@gmail.com>
2026-01-29 18:58:23 +0100

    feat(back/chat): wire chat controller and redis module

--- apps/backend/src/modules/chat/chat.module.ts
+++ apps/backend/src/modules/chat/chat.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { NatsModule } from '@modules/chat/nats/nats.module';
 import { IngestUseCase } from '@modules/chat/ingest/ingest.usecase';
 import { IngestSubscriberService } from '@modules/chat/ingest/ingest.subscriber';
+import { RedisModule } from '@modules/redis/redis.module';
+import { ChatController } from '@modules/chat/chat.controller';
 
 @Module({
-    imports: [NatsModule],
+    imports: [NatsModule, RedisModule],
+    controllers: [ChatController],
     providers: [IngestUseCase, IngestSubscriberService],
 })
 export class ChatModule {}
