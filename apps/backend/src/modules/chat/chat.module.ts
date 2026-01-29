ae9fde803da19c600c9e175f343a9710706fbf5e
kannoqwe <pyntoxfejk@gmail.com>
2026-01-29 15:06:20 +0100

    feat(back/chat): add chat module

--- apps/backend/src/modules/chat/chat.module.ts
+++ apps/backend/src/modules/chat/chat.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NatsModule } from '@modules/chat/nats/nats.module';
+import { IngestUseCase } from '@modules/chat/ingest/ingest.usecase';
+import { IngestSubscriberService } from '@modules/chat/ingest/ingest.subscriber';
 
-@Module({})
+@Module({
+    imports: [NatsModule],
+    providers: [IngestUseCase, IngestSubscriberService],
+})
 export class ChatModule {}
