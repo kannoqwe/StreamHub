229a37c6ea03da707bb53d8e059af610f5c0759d
kannoqwe <pyntoxfejk@gmail.com>
2026-02-07 16:30:08 +0100

    fix(back/user): import jwt module for follow guard

--- apps/backend/src/modules/user/user.module.ts
+++ apps/backend/src/modules/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserRepository } from '@modules/user/user.repository';
 import { RedisModule } from '@modules/redis/redis.module';
 import { UserController } from '@modules/user/user.controller';
 import { FollowModule } from '@modules/follow/follow.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [RedisModule, FollowModule],
+    imports: [RedisModule, FollowModule, JwtModule],
     controllers: [UserController],
     providers: [UserService, UserRepository],
     exports: [UserService, UserRepository],
